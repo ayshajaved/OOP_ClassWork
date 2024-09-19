5dbf8bb4d7a9300bb5d954b8986c6f00ed6a1b02
Ayesha Javed <ayshajavd@gmail.com>
2024-09-19 17:10:58 +0500

    Add files via upload

--- Assignmnet1/ArtWork.java
+++ Assignmnet1/ArtWork.java
@@ -7,23 +7,25 @@ private int yearOfArtWork;
 //composition
 private Artist artist;
 	//constructor overloading
-public Artwork(String title, int year, Artist artist){
+public ArtWork(String title, int year, Artist artist){
 this.artWorkTitle = title;
 this.yearOfArtWork = year;
 this.artist = artist;
 }
 
-public Artwork(String title, int year){
+public ArtWork(String title, int year){
 this.artWorkTitle = title;
 this.yearOfArtWork = year;
 artist = new Artist();
 }
 //copy constructor
 ArtWork(ArtWork art){
-	this.artWorkTitle = getArtWorkTitle();
-	this.yearOfArtWork = getArtWorkYear();
-
+    this.artWorkTitle = art.getArtWorkTitle(); // Correct logic
+    this.yearOfArtWork = art.getArtWorkYear();
+    this.artist = new Artist();  // Deep copy of the artist
+    this.artist.setArtistname(art.getArtist()); // Copy artist's name
 }
+
 //getters
 public String getArtWorkTitle(){
 	return artWorkTitle;
@@ -39,5 +41,4 @@ return artist.getArtistname();
 public void displayArtWork(){
 System.out.println("The ArtWork is: "+getArtWorkTitle()+"\n"+"It is made in year: "+getArtWorkYear()+"\n"+"This Masterpiece is made by: "+getArtist());
 }
-
 }
--- Assignmnet1/Driver.java
+++ Assignmnet1/Driver.java
@@ -1,40 +1,31 @@
-public class Driver{
-	public static void main(String []args){
-Artist a1 = new Artist();
-a1.displayArtistinfo();
-ArtWork artwork1 = new ArtWork("Ayat-ul-kursi", 2000, a1);
-artwork1.displayArtWork();
-Artwork artwork2 = new ArtWork("Natural mountain Sceenery", 2010);
-artwork2.displayArtWork();
+public class Driver {
+    public static void main(String[] args) {
+        Artist a1 = new Artist();
+        a1.displayArtistinfo();
+        // Create artwork objects
+        ArtWork artwork1 = new ArtWork("Ayat-ul-Kursi", 2000, a1);
+        artwork1.displayArtWork();
+        System.out.println();
+        ArtWork artwork2 = new ArtWork("Natural mountain Scenery", 2010);
+        artwork2.displayArtWork();
+        System.out.println();
+        // Deep copying artwork
+        ArtWork artwork3 = new ArtWork(artwork1);
+        artwork3.displayArtWork();
+        System.out.println();        
+        // Shallow copying artwork
+        ArtWork artwork4 = artwork1;
+        artwork4.displayArtWork();
+        System.out.println();
+        // Modify artist's name and see changes
+        a1.setArtistname("Maheen Fatima");
+        System.out.println("Printing changed Artist Artwork!");
+        artwork1.displayArtWork();
+        System.out.println("--------------------");
+        artwork2.displayArtWork();
+        System.out.println("--------------------");
+        artwork3.displayArtWork();
+        System.out.println("--------------------");
 
-//deep copying artwork
-ArtWork artwork3 = new ArtWork(artwork1);
-artwork3.displayArtWork();
-
-//shallow copying artwork
-ArtWork artwork4 = artwork1;
-artwork4.displayArtWork();
-
-a1.setArtistname("Maheen fatima");
-artwork1.displayArtWork();
-artwork2.displayArtWork();
-artwork3.displayArtWork();
-
-
-
-
-
-}
-	
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
